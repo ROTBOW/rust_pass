bcac66a95967f5adfb70c319896a48b76fb98550
Josiah Leon <sjosiahleon@gmail.com>
2025-02-05 16:24:52 -0800

    got command structure setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use clap::{Parser, Subcommand};
+
+/// simple password manager
+#[derive(Parser, Debug)]
+#[command(version = "0.0.1", about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// saying hi
+    Greet {
+        /// name of the person
+        #[arg(short, long)]
+        name: String,
+
+        /// count times to say it
+        #[arg(short, long, default_value_t = 1)]
+        count: u8,
+    },
+
+    /// Check if your logged in
+    Check {},
+
+
+}
+
+
+/// COMMANDS ///
+fn greet(name: String, count: u8) -> () {
+    for _ in 0..count {
+        println!("hey {}!", name)
+    }
+}
+
+
 fn main() {
-    println!("woah there buddy");
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Greet { name, count } => {
+            greet(name, count)
+        }
+        Commands::Check {} => {
+            println!("woah")
+        }
+    }
 }
